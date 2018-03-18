cbd163fab5d8ef40ef3c8ef9c8e98ce44c02ac27
Chris Pearce <chris@pearce.org.nz>
2018-03-18 22:12:33 +1300

    Fix ItemsetSupport to correctly report support.
    ItemsetSupport was using a hashmap with an integer key mapping to the
    support. The integer key was the hashcode of the items array. This was
    incorrect, as multiple arrays can hash to the same hash code, so when
    collisions occurred in the hash table, the old support value would be
    overwritten, causing multiple itemsets to have the same supports, and one
    would be incorrect.
    
    So change to using an ArrayList as the key.

--- src/main/java/nz/org/pearce/arm/ItemsetSupport.java
+++ src/main/java/nz/org/pearce/arm/ItemsetSupport.java
@@ -6,18 +6,28 @@ import java.util.HashMap;
 
 public class ItemsetSupport
 {
-  private HashMap<Integer, Double> map = new HashMap<Integer, Double>();
+  private HashMap<ArrayList<Integer>, Double> map =
+    new HashMap<ArrayList<Integer>, Double>();
 
   public static ItemsetSupport make(ArrayList<FrequentPattern> patterns,
                                     int numTransactions)
   {
     ItemsetSupport supports = new ItemsetSupport();
     for (FrequentPattern pattern : patterns) {
-      supports.map.put(new Integer(Arrays.hashCode(pattern.itemset)),
+      supports.map.put(toArrayList(pattern.itemset),
                        (double)pattern.count / (double)numTransactions);
     }
     return supports;
   }
 
-  public double support(int[] items) { return map.get(Arrays.hashCode(items)); }
+  public double support(int[] items) { return map.get(toArrayList(items)); }
+
+  private static ArrayList<Integer> toArrayList(int[] items)
+  {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    for (int i : items) {
+      list.add(i);
+    }
+    return list;
+  }
 }
